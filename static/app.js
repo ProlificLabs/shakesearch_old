2ff3de100265910ecd329eeb1ef1feb7bb9b8a94
Mark Erdmann <mark@pulley.com>
2020-11-24 22:39:17 -0700

    first commit

---
+++ static/app.js
@@ -0,0 +1,39 @@
+class Teller {
+  tell() {
+    console.log("telling...");
+  }
+}
+
+window.onload = () => {
+  let t = new Teller();
+  t.tell();
+};
+
+// main app controller
+const Controller = {
+  search: (ev) => {
+    ev.preventDefault();
+    const form = document.getElementById("form");
+    const data = Object.fromEntries(new FormData(form));
+    const response = fetch(`http://localhost:3001/search?q=${data.query}`).then(
+      (response) => {
+        response.json().then((results) => {
+          console.log(results);
+          Controller.updateTable(results);
+        });
+      }
+    );
+  },
+
+  updateTable: (results) => {
+    const table = document.getElementById("table-body");
+    const rows = [];
+    for (let result of results) {
+      rows.push(`<tr>${result}<tr/>`);
+    }
+    table.innerHTML = rows;
+  },
+};
+
+const form = document.getElementById("form");
+form.addEventListener("submit", Controller.search);
