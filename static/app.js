c378fdeb7d0ae94fb8180a9b9226859afc917a49
Vasanth Sundaravelu <ssuvasanth@gmail.com>
2021-03-13 05:55:51 +0530

    UI changes - dynamic tab
    Signed-off-by: Vasanth Sundaravelu <ssuvasanth@gmail.com>

--- static/app.js
+++ static/app.js
@@ -11,12 +11,46 @@ const Controller = {
   },
 
   updateTable: (results) => {
-    const table = document.getElementById("table-body");
-    const rows = [];
-    for (let result of results) {
-      rows.push(`<tr>${result}<tr/>`);
+       // Define variables
+       var tabs = $('.tabs');
+       var container = $('.container');
+       var html_tabs = '';
+       var html_content = '';
+    console.log(results)
+  var index = 0
+    for (x in results) {
+      html_tabs +='<li><a href="#tab'+index+'">'+x+'</a></li>';
+
+      for (y in results[x]) {
+        for (z in results[x][y])
+        html_content +='<div id="tab'+index+'"><p>'+results[x][y][z]+'</p></div>';
+      }
+
+      index += index + 1;
     }
-    table.innerHTML = rows;
+    tabs.html(html_tabs);
+    container.html(html_content);
+
+    // Set tabs and content html
+    tabs.html(html_tabs);
+    container.html(html_content);
+    // Looping links
+    $.each($('.tabs li a'),function(count, item) {
+       // Set on click handler
+       $(this).on('click',function() {
+          // Hide all div content
+          container.find('div').removeClass('active');
+          var current = $(this).attr('href');
+          // Remove active class on links
+          $('.tabs li a').removeClass('active');
+          // Set active class on current and ul parent
+          $(this).addClass('active');
+          $(this).parents('ul').find('li').removeClass('active');
+          $(this).parent().addClass('active');
+          // Show current container
+          $(current).addClass('active');
+       });  
+    }).eq(1).click().addClass('active');    
   },
 };
 
