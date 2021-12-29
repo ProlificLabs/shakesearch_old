f8f31ed3eca875d3e6b346dd13944d98aaabbce6
Steven Vachon <contact@svachon.com>
2021-12-29 15:05:37 -0500

    Minor changes

--- static/app.js
+++ static/app.js
@@ -3,7 +3,7 @@ const Controller = {
     ev.preventDefault();
     const form = document.getElementById("form");
     const data = Object.fromEntries(new FormData(form));
-    const response = fetch(`/search?q=${data.query}`).then((response) => {
+    fetch(`/search?q=${data.query}`).then((response) => {
       response.json().then((results) => {
         Controller.updateTable(results);
       });
