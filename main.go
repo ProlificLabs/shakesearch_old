62ff58e46f8835ec38277a41adb0e27e346f61b6
Blaine Willhoft <blaine.willhoft@gmail.com>
2022-04-23 16:16:35 -0500

    Working play search

--- main.go
+++ main.go
@@ -13,6 +13,51 @@ import (
 	"strconv"
 )
 
+var playTitles = []string{
+	"ALL’S WELL THAT ENDS WELL",
+	"ANTONY AND CLEOPATRA",
+	"AS YOU LIKE IT",
+	"THE COMEDY OF ERRORS",
+	"THE TRAGEDY OF CORIOLANUS",
+	"CYMBELINE",
+	"THE TRAGEDY OF HAMLET, PRINCE OF DENMARK",
+	"THE FIRST PART OF KING HENRY THE FOURTH",
+	"THE SECOND PART OF KING HENRY THE FOURTH",
+	"THE LIFE OF KING HENRY V",
+	"THE FIRST PART OF HENRY THE SIXTH",
+	"THE SECOND PART OF KING HENRY THE SIXTH",
+	"THE THIRD PART OF KING HENRY THE SIXTH",
+	"KING HENRY THE EIGHTH",
+	"KING JOHN",
+	"THE TRAGEDY OF JULIUS CAESAR",
+	"THE TRAGEDY OF KING LEAR",
+	"LOVE’S LABOUR’S LOST",
+	"MACBETH",
+	"MEASURE FOR MEASURE",
+	"THE MERCHANT OF VENICE",
+	"THE MERRY WIVES OF WINDSOR",
+	"A MIDSUMMER NIGHT’S DREAM",
+	"MUCH ADO ABOUT NOTHING",
+	"OTHELLO, THE MOOR OF VENICE",
+	"PERICLES, PRINCE OF TYRE",
+	"KING RICHARD THE SECOND",
+	"KING RICHARD THE THIRD",
+	"THE TRAGEDY OF ROMEO AND JULIET",
+	"THE TAMING OF THE SHREW",
+	"THE TEMPEST",
+	"THE LIFE OF TIMON OF ATHENS",
+	"THE TRAGEDY OF TITUS ANDRONICUS",
+	"THE HISTORY OF TROILUS AND CRESSIDA",
+	"TWELFTH NIGHT: OR, WHAT YOU WILL",
+	"THE TWO GENTLEMEN OF VERONA",
+	"THE TWO NOBLE KINSMEN",
+	"THE WINTER’S TALE",
+	"A LOVER’S COMPLAINT",
+	"THE PASSIONATE PILGRIM",
+	"THE PHOENIX AND THE TURTLE",
+	"THE RAPE OF LUCRECE",
+	"VENUS AND ADONIS"}
+
 func main() {
 	searcher := Searcher{}
 	err := searcher.Load("completeworks.txt")
@@ -79,9 +124,16 @@ func (s *Searcher) Load(filename string) error {
 	s.CompleteWorks = string(dat)
 	s.SuffixArray = suffixarray.New(dat)
 
-	sonnetStart := regexp.MustCompile(`\nTHE SONNETS`).FindStringIndex(s.CompleteWorks)[1]
-	sonnetEnd := regexp.MustCompile(`\nTHE END`).FindStringIndex(s.CompleteWorks)[0]
-	sonnets := regexp.MustCompile(`(?:[^0-9\r\n]+\r\n)+`).FindAllString(s.CompleteWorks[sonnetStart:sonnetEnd], -1)
+	s.loadSonnets()
+	s.loadPlays()
+
+	return nil
+}
+
+func (s *Searcher) loadSonnets() {
+	sonnetsStart := regexp.MustCompile(`\nTHE SONNETS`).FindStringIndex(s.CompleteWorks)[1]
+	sonnetsEnd := regexp.MustCompile(`\nTHE END`).FindStringIndex(s.CompleteWorks)[0]
+	sonnets := regexp.MustCompile(`(?:[^0-9\r\n]+\r\n)+`).FindAllString(s.CompleteWorks[sonnetsStart:sonnetsEnd], -1)
 	for index, sonnet := range sonnets {
 		work := Work{}
 		work.Title = "Sonnet " + strconv.Itoa(index + 1)
@@ -89,8 +141,28 @@ func (s *Searcher) Load(filename string) error {
 		work.SuffixArray = suffixarray.New([]byte(sonnet))
 		s.Works = append(s.Works, work);
 	}
+}
 
-	return nil
+func (s *Searcher) loadPlays() {
+	playsStart := regexp.MustCompile(`\nTHE END`).FindStringIndex(s.CompleteWorks)[1]
+	playsEnd := regexp.MustCompile(`CONTENT NOTE`).FindStringIndex(s.CompleteWorks)[0]
+
+	for titleIndex, title := range playTitles {
+		start := regexp.MustCompile(title).FindStringIndex(s.CompleteWorks[playsStart:playsEnd])[1]
+		var end int
+		if (titleIndex == len(playTitles) - 1) {
+			end = playsEnd
+		} else {
+			end = regexp.MustCompile(playTitles[titleIndex + 1]).FindStringIndex(s.CompleteWorks[playsStart:playsEnd])[0]
+		}
+		text := s.CompleteWorks[start:end]
+
+		work := Work{}
+		work.Title = title
+		work.Text = text
+		work.SuffixArray = suffixarray.New([]byte(text))
+		s.Works = append(s.Works, work);
+	}
 }
 
 func (s *Searcher) Search(query string) []string {
