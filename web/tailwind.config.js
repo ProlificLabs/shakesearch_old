a3c83133dfe792ebad2115071c2b12d312228c66
Daniel Ferreira <danielferreira4007@gmail.com>
2022-05-20 08:17:25 -0400

    Add React

---
+++ web/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'quote': ['Roboto Mono', 'monospace']
+    }
+  },
+  plugins: [],
+}
