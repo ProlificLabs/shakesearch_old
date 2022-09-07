ae603c48d3364a1341d9bddae5051cad3efc9755
Max <max.museur@gmail.com>
2022-09-07 13:26:30 -0700

    Deploy off-line changes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/json"
 	"fmt"
@@ -9,7 +10,21 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
+
+const (
+	AMOUNT_OF_LINES int = 169432
+	START_OF_WORKS  int = 133
+)
+
+var END_OF_EACH_WORK []int = []int{2909, 7873, 14514, 17306, 20507, 24624, 30499, 37187,
+	41902, 45311, 50247, 53518, 57006, 60352, 64001, 66918, 71562, 77598, 80513,
+	84662, 87663, 91831, 94789, 98249, 103961, 110231, 114369, 117467, 121875,
+	127131, 132005, 135828, 138544, 141420, 147583, 152066, 154478, 159666,
+	164681, 165064, 165304, 165398, 167583, 169017}
 
 func main() {
 	searcher := Searcher{}
@@ -22,6 +37,9 @@ func main() {
 	http.Handle("/", fs)
 
 	http.HandleFunc("/search", handleSearch(searcher))
+	http.HandleFunc("/add-lines/up", handleRequestAddLines(true, searcher))
+	http.HandleFunc("/add-lines/down", handleRequestAddLines(false, searcher))
+	http.HandleFunc("/read-work", handleReadWork(searcher))
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -36,19 +54,156 @@ func main() {
 }
 
 type Searcher struct {
-	CompleteWorks string
-	SuffixArray   *suffixarray.Index
+	CompleteWorks    string
+	SuffixArray      *suffixarray.Index
+	SearchLines      []SearchLine
+	EndOfLineIndexes []int
+}
+
+type SearchLine struct {
+	TextResult string
+	LineIndex  uint
+}
+
+func (s *SearchLine) setTextResult(newTextResult string) {
+	s.TextResult = newTextResult
+}
+
+type ResultParagraph struct {
+	Paragraph []SearchLine
+}
+
+func handleReadWork(searcher Searcher) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		lineNumberString, ok := r.URL.Query()["line"]
+		if !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Wrong request, make sure to add line-number in URL."))
+			return
+		}
+		queryArray, ok2 := r.URL.Query()["q"]
+		if !ok2 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Wrong request, make sure to add query in URL."))
+			return
+		}
+		query := queryArray[0]
+		lineNumber, error := strconv.Atoi(lineNumberString[0])
+		if error != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("The lineNumber in the URL is not a number..."))
+			return
+		}
+		if lineNumber < START_OF_WORKS {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("That paragraph is not part of a book. It belongs to the introduction of the complete works."))
+			return
+		}
+
+		workId := getWorkIdOfLine(lineNumber)
+		if workId == -1 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("That paragraph is not part of a book. It belongs to the ending of the complete works. "))
+			return
+		}
+		allLines := getLinesByWorkId(workId, searcher, query)
+
+		results := []ResultParagraph{ResultParagraph{Paragraph: allLines}} // this is a slice because frontend expects an iterable
+		buf := &bytes.Buffer{}
+		enc := json.NewEncoder(buf)
+		err := enc.Encode(results)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("encoding failure"))
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(buf.Bytes())
+	}
+}
+
+func getWorkIdOfLine(lineNumber int) int {
+	var workId int = -1
+	for i := 0; i < len(END_OF_EACH_WORK); i++ {
+		if lineNumber < END_OF_EACH_WORK[i] {
+			workId = i
+			break
+		}
+	}
+	return workId
+}
+
+func getLinesByWorkId(workId int, s Searcher, query string) []SearchLine {
+	allLines := []SearchLine{}
+	beginning := START_OF_WORKS
+	if workId != 0 {
+		beginning = END_OF_EACH_WORK[workId-1] + 1
+	}
+	end := END_OF_EACH_WORK[workId]
+
+	for i := beginning; i < end; i++ {
+		allLines = append(allLines, s.SearchLines[i].highlightRegexQuery(query))
+	}
+	return allLines
+}
+
+func handleRequestAddLines(addLinesUp bool, searcher Searcher) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r)
+		lineNumberList, ok := r.URL.Query()["q"]
+		if !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Wrong request, make sure to add query in URL."))
+			return
+		}
+		lineNumber, error := (strconv.Atoi(lineNumberList[0]))
+		if error != nil {
+			fmt.Println(error)
+		}
+
+		if lineNumber >= AMOUNT_OF_LINES || lineNumber <= 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Line number is higher than amount of lines in document..."))
+			return
+		}
+
+		extraLines := getExtraLines(addLinesUp, lineNumber, searcher)
+
+		buf := &bytes.Buffer{}
+		enc := json.NewEncoder(buf)
+		err := enc.Encode(extraLines)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("encoding failure"))
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(buf.Bytes())
+	}
+}
+
+func getExtraLines(addLinesUp bool, lineNumber int, searcher Searcher) []SearchLine {
+	extraLines := []SearchLine{}
+	for i := 1; i <= 3; i++ {
+		if addLinesUp && lineNumber-i >= 0 {
+			extraLines = append(extraLines, searcher.SearchLines[lineNumber-i])
+		} else if !addLinesUp && lineNumber+i <= AMOUNT_OF_LINES {
+			extraLines = append(extraLines, searcher.SearchLines[lineNumber+i])
+		}
+	}
+	return extraLines
 }
 
 func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r)
 		query, ok := r.URL.Query()["q"]
 		if !ok || len(query[0]) < 1 {
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte("missing search query in URL params"))
 			return
 		}
-		results := searcher.Search(query[0])
+		results := searcher.SearchWithRegex(query[0])
 		buf := &bytes.Buffer{}
 		enc := json.NewEncoder(buf)
 		err := enc.Encode(results)
@@ -63,20 +218,134 @@ func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request
 }
 
 func (s *Searcher) Load(filename string) error {
-	dat, err := ioutil.ReadFile(filename)
+
+	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return fmt.Errorf("Load: %w", err)
 	}
-	s.CompleteWorks = string(dat)
-	s.SuffixArray = suffixarray.New(dat)
+
+	lines, endOfLineIndexes := loadLinesInfo(filename)
+
+	s.EndOfLineIndexes = endOfLineIndexes
+	s.SearchLines = lines
+	s.CompleteWorks = string(data)
+	s.SuffixArray = suffixarray.New(data)
 	return nil
 }
 
-func (s *Searcher) Search(query string) []string {
-	idxs := s.SuffixArray.Lookup([]byte(query), -1)
-	results := []string{}
-	for _, idx := range idxs {
-		results = append(results, s.CompleteWorks[idx-250:idx+250])
+func loadLinesInfo(filename string) ([]SearchLine, []int) {
+	file, _ := os.Open(filename)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lines := []SearchLine{}
+	lineIndex := 0
+	currentEndOfLineIndex := 0
+	endOfLineIndexes := []int{}
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		currentEndOfLineIndex += len(text) + 2 // The + 2 is because the delimiter of the scanner (/n) is left out
+		endOfLineIndexes = append(endOfLineIndexes, currentEndOfLineIndex)
+
+		searchLine := SearchLine{
+			TextResult: text,
+			LineIndex:  uint(lineIndex),
+		}
+		lines = append(lines, searchLine)
+		lineIndex++
+	}
+
+	return lines, endOfLineIndexes
+}
+
+func (s *Searcher) SearchWithRegex(query string) []ResultParagraph {
+	re := regexp.MustCompile(query)
+	restultIndeces := re.FindAllStringIndex(s.CompleteWorks, -1)
+	allSearchLinesIndeces := getSearchLinesByIndex(restultIndeces, s)
+	allParagraphs := makeParagraphsOutOfLines(allSearchLinesIndeces, s, query)
+	return allParagraphs
+}
+
+func makeParagraphsOutOfLines(searchLinesIndeces []int, s *Searcher, query string) []ResultParagraph {
+	allParagraphs := []ResultParagraph{}
+
+	for _, lineIndex := range searchLinesIndeces {
+		paragraph := []SearchLine{}
+		if lineIndex != 0 {
+			paragraph = append(paragraph, s.SearchLines[lineIndex-1].highlightRegexQuery(query))
+		}
+
+		paragraph = append(paragraph, s.SearchLines[lineIndex].highlightRegexQuery(query))
+
+		if lineIndex != AMOUNT_OF_LINES-2 {
+			paragraph = append(paragraph, s.SearchLines[lineIndex+1].highlightRegexQuery(query))
+		}
+
+		allParagraphs = append(allParagraphs, ResultParagraph{Paragraph: paragraph})
+	}
+
+	return allParagraphs
+}
+
+func getSearchLinesByIndex(indecesOfMatches [][]int, s *Searcher) []int {
+	currentLine := 0
+	results := []int{}
+
+	for _, indexOfMatch := range indecesOfMatches {
+		for currentLine < len(s.EndOfLineIndexes) {
+			if indexOfMatch[0] < s.EndOfLineIndexes[currentLine] {
+				results = append(results, currentLine)
+				break
+			} else {
+				currentLine++
+			}
+		}
 	}
 	return results
+}
+
+func (s *Searcher) Search(query string) []ResultParagraph {
+	results := []ResultParagraph{}
+
+	for i, searchLine := range s.SearchLines {
+		if strings.Contains(searchLine.TextResult, query) {
+			if i == 0 {
+				i = 1 // to prevent indexOutOfBoundsError
+			}
+
+			searchLines := []SearchLine{
+				s.SearchLines[i-1].highlightQuery(query),
+				s.SearchLines[i].highlightQuery(query),
+				s.SearchLines[i+1].highlightQuery(query),
+			}
+
+			resultParagaph := ResultParagraph{
+				Paragraph: searchLines,
+			}
+
+			results = append(results, resultParagaph)
+		}
+	}
+
+	return results
+}
+
+func (s *SearchLine) highlightQuery(query string) SearchLine {
+	newText := strings.ReplaceAll(s.TextResult, query, `<span style="color: #FD5F00;">`+query+`</span>`)
+	s.setTextResult(newText)
+	return *s
+}
+
+func (s *SearchLine) highlightRegexQuery(query string) SearchLine {
+	re := regexp.MustCompile(query)
+	newText := re.ReplaceAllStringFunc(s.TextResult,
+		func(original string) string {
+			return `<span style="color: #FD5F00;">` + original + `</span>`
+		},
+	)
+	return SearchLine{
+		TextResult: newText,
+		LineIndex:  s.LineIndex,
+	}
 }
--- static/app.js
+++ static/app.js
@@ -1,24 +1,209 @@
 const Controller = {
   search: (ev) => {
+    // remove previous search results
+    const searchList = document.getElementById("search-list");
+    searchList.innerHTML = "";
+    const statusInfo = document.getElementById("status-info");
+    statusInfo.innerHTML = "";
+
+    // add spinner
+    const spinner = document.createElement("div");
+    spinner.setAttribute("id", "spinner");
+    spinner.classList.add("spinner");
+    searchList.prepend(spinner);
+
     ev.preventDefault();
     const form = document.getElementById("form");
     const data = Object.fromEntries(new FormData(form));
     const response = fetch(`/search?q=${data.query}`).then((response) => {
+      if (response.status === 400) {
+        // display error message
+        const statusInfo = document.getElementById("status-info");
+        const errorMessage = document.createElement("h3");
+        errorMessage.innerHTML = `<span style="color: #FD5F00;">Your search did not match anything. </span> <br>
+          <br> Suggestions: 
+           <br> Make sure all words are spelled correctly. 
+           <br> Try different keywords.
+           <br> Try more general keywords.
+           <br> Try fewer keywords.`;
+        statusInfo.prepend(errorMessage);
+      }
       response.json().then((results) => {
-        Controller.updateTable(results);
+        Controller.updateTable(results, data.query);
+        try {
+          spinner.parentElement.removeChild(spinner);
+        } catch (err) {
+          // do nothing because that means spinner was already removed
+        }
+      })
+      .catch(error => {
+        if (error instanceof SyntaxError) {
+          // do nothing because this happens when the query has no matches
+        } 
+        else {
+          const statusInfo = document.getElementById("status-info");
+          statusInfo.innerHTML = "";
+          const errorMessage = document.createElement("h3");
+          errorMessage.innerHTML = `<span style="color: #FD5F00;">Something went wrong. Try again later... </span>`;
+          statusInfo.prepend(errorMessage);
+          console.log(error);
+        }
       });
     });
-  },
+  }, 
 
-  updateTable: (results) => {
-    const table = document.getElementById("table-body");
-    const rows = [];
+  updateTable: (results, query) => {
+    const searchList = document.getElementById("search-list");
+    searchList.innerHTML = ''
     for (let result of results) {
-      rows.push(`<tr>${result}<tr/>`);
+      const paragraph = result.Paragraph;
+
+      
+      let text = formatParagraph(paragraph);
+      const firstLine = paragraph[0];
+      const lastLine = paragraph[paragraph.length - 1];
+
+      searchList.insertAdjacentHTML( 'beforeend', `<li id=${firstLine.LineIndex + "-" + lastLine.LineIndex}><div class="card">
+          <pre>${text}  <pre/>
+        <div/> <li/>`);
+
+      console.log("searchList: ");
+      console.log(searchList);
+      const preList = searchList.getElementsByTagName('pre')
+
+      // delete pre element that got added by joining <li> elements
+      const redundantPre = preList[preList.length - 1];
+      redundantPre.parentNode.removeChild(redundantPre);
     }
-    table.innerHTML = rows;
+
+    const cards = document.getElementsByClassName("card");
+    for (let i = 0; i < cards.length; i++) {
+
+      let button1 = document.createElement("button");
+      let button2 = document.createElement("button");
+
+      const card = cards[i]
+      card.insertBefore(button1, card.firstChild)
+      card.appendChild(button2)
+
+
+      if (cards[i].parentElement.id.split("-")[0] !== "0") {
+        button1.innerHTML = "...";
+        button1.classList.add("add-lines");
+        button1.addEventListener("click", function(event) {
+          console.log("button1 is clicked");
+          handleAddLinesClick(event, true);
+        });
+      } else {
+        // delete button1
+        button1.parentNode.removeChild(button1);
+      }
+
+      
+      if (parseInt(cards[i].parentElement.id.split("-")[1]) < 169432) {
+        button2.innerHTML = "...";
+        button2.classList.add("add-lines");
+        button2.addEventListener("click", function(event) {
+          console.log("button2 is clicked");
+          handleAddLinesClick(event, false);
+        });
+      } else {
+        // delete button1
+        button2.parentNode.removeChild(button2);
+      }
+
+      // add link to read entire work
+      const lineNumbers = card.parentElement.id.split("-");
+      const middleLine = Math.ceil((lineNumbers[0] + lineNumbers[1]) / 2)
+      const openWork = `<div class="open-work-wrapper"><button class="open-work">Open Book</button></div>`;
+      card.insertAdjacentHTML("beforeend", openWork)
+
+      const openBookButtons = card.getElementsByClassName("open-work");
+      for (let i = 0; i < openBookButtons.length; i++) {
+        openBookButtons[i].addEventListener("click", (event) => {
+          console.log("clicked Open book button")
+          handleOpenWorkClick(event, query);
+        })
+      }
+    }
   },
 };
 
+const handleOpenWorkClick = (event, query) => {
+  const startAndEndId = event.target.parentElement.parentElement.parentElement.id.split("-");
+
+  const middleLine = Math.ceil((parseInt(startAndEndId[0]) + parseInt(startAndEndId[1])) / 2);
+  const url = `/read-work?q=${query}&line=${middleLine}`;
+
+  fetch(url).then((response) => {
+    response
+    .json()
+    .then((results) => {
+      Controller.updateTable(results, query);
+    });
+  })
+}
+
+const formatParagraph = (paragraph) => {
+  let text = ""
+  for (let i = 0; i < paragraph.length; i++) {
+    text += paragraph[i].TextResult + "\n"
+  }
+  return text
+}
+
 const form = document.getElementById("form");
 form.addEventListener("submit", Controller.search);
+
+
+const handleAddLinesClick = (event, addLinesUp) => {
+  const wrapper = event.target.parentElement.parentElement
+  wrapperId = wrapper.id;
+  const lineIndexes = wrapperId.split("-");
+  const url = addLinesUp ? `/add-lines/up?q=${lineIndexes[0]}` : `/add-lines/down?q=${lineIndexes[1]}`;
+
+  fetch(url).then((response) => {
+    response
+    .json()
+    .then((result) => {
+      addExtraLines(wrapper, event, addLinesUp, result);
+    });
+  });
+  
+}
+
+const addExtraLines = (wrapper, event, addLinesUp, result) => {
+  const card = event.target.parentElement
+  const pre = card.getElementsByTagName("pre")[0]
+
+  if (addLinesUp) {
+    result = result.reverse();
+  } 
+  const text = formatParagraph(result);
+  console.log("text: " + text);
+  if (addLinesUp) {
+    // Change text to add new lines
+    pre.innerHTML = text + pre.innerHTML;
+    
+    // Add indexes of lines of that card
+    let newId = wrapper.id.split("-");
+    newId[0] = result[0].LineIndex;
+    wrapper.id = newId.join("-");
+    if (parseInt(newId[0]) == 0) {
+      // remove button
+      const button = card.getElementsByTagName("button")[0];
+      button.parentElement.removeChild(button);
+    }
+  } else {
+    pre.innerHTML = pre.innerHTML + text;
+    let newId = wrapper.id.split("-");
+    newId[1] = result[result.length-1].LineIndex;
+    wrapper.id = newId.join("-");
+
+    if (parseInt(newId[1]) == 169432) {
+      // remove button 
+      const button = card.getElementsByTagName("button")[1];
+      button.parentElement.removeChild(button);
+    }
+  }
+}
