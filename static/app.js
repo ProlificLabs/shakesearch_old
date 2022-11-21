9008bb56d5fac50485465f35ca8612886f106c85
andrralv <andrralv@gmail.com>
2022-11-21 10:53:23 -0600

    andrralv-01: initial changes and CSS -FE

--- static/app.js
+++ static/app.js
@@ -1,22 +1,36 @@
 const Controller = {
   search: (ev) => {
     ev.preventDefault();
+
     const form = document.getElementById("form");
     const data = Object.fromEntries(new FormData(form));
-    const response = fetch(`/search?q=${data.query}`).then((response) => {
-      response.json().then((results) => {
-        Controller.updateTable(results);
-      });
+    fetch(`/search?q=${data.query}`).then((response) => {
+      if (response.ok) {
+        response.json().then((results) => {
+          Controller.updateTable(results);
+        });
+      } else {
+        console.error("Search query cannot be empty");
+      }
     });
+
+    (async () => {
+      const offsetTop = document.querySelector("#shake-search-title").offsetTop;
+      await scroll({
+        top: offsetTop - 20,
+        behavior: "smooth"
+      });
+    })();
   },
 
   updateTable: (results) => {
-    const table = document.getElementById("table-body");
-    const rows = [];
-    for (let result of results) {
-      rows.push(`<tr>${result}<tr/>`);
-    }
-    table.innerHTML = rows;
+    const list = document.getElementById("quote-list");
+    const summary = document.getElementById("results-summary");
+    const rows = results.map(result => `<li class="quote">...${result}...<li/>`)
+    const sanitizedRows = rows.join("");
+    console.log(sanitizedRows);
+    list.innerHTML = sanitizedRows;
+    summary.innerHTML = `Search Results: (${rows.length})`
   },
 };
 
