d7fdef47d084fcb1878b8341fae1fec6ec5cc089
Eric Magalhães <eric@magalhaes.com.br>
2023-03-13 21:30:59 -0300

    added summary

--- static/app.js
+++ static/app.js
@@ -1,22 +1,43 @@
 const Controller = {
   search: (ev) => {
     ev.preventDefault();
+
+    const summary = document.getElementById("summary");
+    summary.innerHTML = "";
+
     const form = document.getElementById("form");
     const data = Object.fromEntries(new FormData(form));
-    const response = fetch(`/search?q=${data.query}`).then((response) => {
-      response.json().then((results) => {
-        Controller.updateTable(results);
+
+    if (data.query != "") {
+      const response = fetch(`/search?q=${data.query}`).then((response) => {
+        response.json().then((results) => {
+          Controller.updateTable(results);
+        });
       });
-    });
+    } else {
+      summary.textContent = "No search term was given.";
+    }
   },
 
   updateTable: (results) => {
+    const summary = document.getElementById("summary");
+    switch(results.length) {
+      case 0:
+        summary.textContent = "No results were found.";
+        break;
+      case 1:
+        summary.textContent = "One result was found.";
+        break;
+      default:
+        summary.textContent = `${results.length} results were found.`;
+    }
+
     const table = document.getElementById("table-body");
     const rows = [];
     for (let result of results) {
       rows.push(`<tr><p class="result">${result}</p><tr/>`);
     }
-    table.innerHTML = rows;
+    table.innerHTML = rows.join();
   },
 };
 
