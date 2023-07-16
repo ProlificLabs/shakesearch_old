f74df29cfd66de8353c7be83ae6c808cf079eab6
Diego <diegodf@gmail.com>
2023-07-16 14:28:29 -0400

    Fix failing test and improve search functionality with pagination and case-insensitive search
    This commit addresses a failing test and enhances the search experience by introducing
    search result pagination and case-insensitive search.
    
    The failing test has been fixed by adjusting the search and load functions in the Searcher
    struct, allowing the tests to successfully run as intended.
    
    Pagination has been added to improve performance and user experience when dealing with
    large result sets. The pagination parameters (limit and offset) are now included in the
    Searcher struct and handleSearch function, allowing users to retrieve a specific subset
    of search results.
    
    Case-insensitive searching has been introduced, with both the CompleteWorks text and search
    queries converted to lowercase, ensuring that the search function is more robust and user-friendly.
    
    On the client-side, JavaScript has been updated to pass the pagination parameters to the search
    endpoint and a "Load More" button has been added. This button allows users to retrieve more
    search results without re-executing the entire search query and only appears when there are more
    results available to load.
    
    Modified files:
    - main.go
    - static/app.js
    - static/index.html

--- static/app.js
+++ static/app.js
@@ -1,24 +1,45 @@
 const Controller = {
+  limit: 20,  // Number of results to display per fetch
+  offset: 0,  // Starting offset
+
   search: (ev) => {
     ev.preventDefault();
     const form = document.getElementById("form");
     const data = Object.fromEntries(new FormData(form));
-    const response = fetch(`/search?q=${data.query}`).then((response) => {
+    const response = fetch(`/search?q=${data.query}&limit=${Controller.limit}&offset=${Controller.offset}`).then((response) => {
       response.json().then((results) => {
         Controller.updateTable(results);
+
+        // Display the load-more button if results were found.
+        if (results.length > 0) {
+          document.getElementById("load-more").style.display = 'block';
+        }
       });
     });
   },
 
   updateTable: (results) => {
     const table = document.getElementById("table-body");
-    const rows = [];
+    if(Controller.offset === 0) table.innerHTML = ""; // Clear the table when it's a new search
     for (let result of results) {
-      rows.push(`<tr><td>${result}</td></tr>`);
+      const row = document.createElement('tr');
+      const cell = document.createElement('td');
+      cell.textContent = result;
+      row.appendChild(cell);
+      table.appendChild(row);
     }
-    table.innerHTML = rows;
   },
 };
 
+
 const form = document.getElementById("form");
-form.addEventListener("submit", Controller.search);
+form.addEventListener("submit", (ev) => {
+  Controller.offset = 0; // Reset offset when a new search is performed
+  Controller.search(ev);
+});
+
+const loadMoreButton = document.getElementById("load-more");
+loadMoreButton.addEventListener("click", () => {
+  Controller.offset += Controller.limit; // Increase offset for each "load more" action
+  Controller.search(new Event('click'));
+});
