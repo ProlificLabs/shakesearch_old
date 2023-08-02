8aa5741b89059ab3f56708b164e70ac77b34ca91
vijayasankarj <vijayasankarj@gmail.com>
2023-08-02 13:54:33 -0700

    fixed failing test cases

--- static/app.js
+++ static/app.js
@@ -1,11 +1,25 @@
 const Controller = {
   search: (ev) => {
     ev.preventDefault();
+    page = 0;
     const form = document.getElementById("form");
     const data = Object.fromEntries(new FormData(form));
-    const response = fetch(`/search?q=${data.query}`).then((response) => {
+    const response = fetch(`/search?q=${data.query}&p=${page}`).then((response) => {
       response.json().then((results) => {
         Controller.updateTable(results);
+        page = page + 1
+      });
+    });
+  },
+
+  loadmore: (ev) => {
+    ev.preventDefault();
+    const form = document.getElementById("form");
+    const data = Object.fromEntries(new FormData(form));
+    const response = fetch(`/search?q=${data.query}&p=${page}`).then((response) => {
+      response.json().then((results) => {
+        Controller.updateTable(results);
+        page = page + 1
       });
     });
   },
@@ -16,9 +30,16 @@ const Controller = {
     for (let result of results) {
       rows.push(`<tr><td>${result}</td></tr>`);
     }
-    table.innerHTML = rows;
+    if (page == 0){
+      table.innerHTML = rows;
+    } else {
+      table.innerHTML = table.innerHTML + rows;
+    }
   },
 };
 
+var page = 0;
 const form = document.getElementById("form");
+const button = document.getElementById("load-more");
 form.addEventListener("submit", Controller.search);
+button.addEventListener("click", Controller.loadmore);
