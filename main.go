63596e79d8535ebb9eec72f4322a5ee771e19f01
Cristian Llorente <17942124+cristiancll@users.noreply.github.com>
2023-08-08 18:21:21 -0300

    implement pagination and fix test cases

--- main.go
+++ main.go
@@ -9,7 +9,55 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 )
+
+type ConfigValues struct {
+	Minimum int
+	Maximum int
+	Default int
+}
+
+type PaginationConfig struct {
+	Offset ConfigValues
+	Limit  ConfigValues
+}
+
+type Pagination struct {
+	Offset int
+	Limit  int
+}
+
+func (p *Pagination) isOffsetValid() bool {
+	return p.Offset >= paginationConfig.Offset.Minimum
+}
+
+func (p *Pagination) isLimitValid() bool {
+	return p.Limit >= paginationConfig.Limit.Minimum && p.Limit <= paginationConfig.Limit.Maximum
+}
+
+func (p *Pagination) validatePagination() {
+	if !p.isOffsetValid() {
+		p.Offset = paginationConfig.Offset.Default
+	}
+	if !p.isLimitValid() {
+		p.Limit = paginationConfig.Limit.Default
+	}
+}
+
+// TODO: load pagination values from config file
+var paginationConfig = PaginationConfig{
+	Offset: ConfigValues{
+		Minimum: 0,
+		Default: 0,
+	},
+	Limit: ConfigValues{
+		Minimum: 1,
+		Maximum: 20,
+		Default: 20,
+	},
+}
 
 func main() {
 	searcher := Searcher{}
@@ -40,6 +88,31 @@ type Searcher struct {
 	SuffixArray   *suffixarray.Index
 }
 
+func getPaginationParams(r *http.Request) (Pagination, error) {
+	var err error
+	var limit, offset int
+	offsetParam, offsetOK := r.URL.Query()["o"]
+	if offsetOK {
+		offset, err = strconv.Atoi(offsetParam[0])
+		if err != nil {
+			return Pagination{}, fmt.Errorf("invalid offset number in URL params: %w", err)
+		}
+	}
+	limitParam, limitOK := r.URL.Query()["l"]
+	if limitOK {
+		limit, err = strconv.Atoi(limitParam[0])
+		if err != nil {
+			return Pagination{}, fmt.Errorf("invalid limit number in URL params: %w", err)
+		}
+	}
+	pagination := Pagination{
+		Offset: offset,
+		Limit:  limit,
+	}
+	pagination.validatePagination()
+	return pagination, nil
+}
+
 func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		query, ok := r.URL.Query()["q"]
@@ -48,10 +121,16 @@ func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request
 			w.Write([]byte("missing search query in URL params"))
 			return
 		}
-		results := searcher.Search(query[0])
+		pagination, err := getPaginationParams(r)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		results := searcher.SearchPaginated(query[0], pagination)
 		buf := &bytes.Buffer{}
 		enc := json.NewEncoder(buf)
-		err := enc.Encode(results)
+		err = enc.Encode(results)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte("encoding failure"))
@@ -67,15 +146,43 @@ func (s *Searcher) Load(filename string) error {
 	if err != nil {
 		return fmt.Errorf("Load: %w", err)
 	}
+	lowerCaseDat := []byte(strings.ToLower(string(dat)))
 	s.CompleteWorks = string(dat)
-	s.SuffixArray = suffixarray.New(dat)
+	s.SuffixArray = suffixarray.New(lowerCaseDat)
 	return nil
 }
 
 func (s *Searcher) Search(query string) []string {
+	pagination := Pagination{
+		Offset: paginationConfig.Offset.Default,
+		Limit:  paginationConfig.Limit.Default,
+	}
+	return s.SearchPaginated(query, pagination)
+}
+
+func (s *Searcher) SearchPaginated(query string, pagination Pagination) []string {
+	if query != "" {
+		query = strings.ToLower(query)
+	}
 	idxs := s.SuffixArray.Lookup([]byte(query), -1)
 	results := []string{}
-	for _, idx := range idxs {
+	if idxs == nil {
+		return results
+	}
+	startIdx := pagination.Offset
+	if startIdx > len(idxs) {
+		return results
+	}
+
+	endIdx := pagination.Offset + pagination.Limit
+	if endIdx > len(idxs) {
+		endIdx = len(idxs)
+	}
+	idxRange := idxs[startIdx:endIdx]
+	if idxRange == nil {
+		return results
+	}
+	for _, idx := range idxRange {
 		results = append(results, s.CompleteWorks[idx-250:idx+250])
 	}
 	return results
--- static/app.js
+++ static/app.js
@@ -1,24 +1,84 @@
+const DEFAULT_OFFSET = 20
+const DEFAULT_LIMIT = 20
+
+const Pagination = {
+  query: "",
+  offset: DEFAULT_OFFSET,
+  limit: DEFAULT_LIMIT,
+  resetPagination(query) {
+    Pagination.query = query;
+    Pagination.offset = DEFAULT_OFFSET;
+    Pagination.limit = DEFAULT_LIMIT;
+  },
+  increasePagination() {
+    Pagination.offset += Pagination.limit;
+  }
+}
+
 const Controller = {
   search: (ev) => {
     ev.preventDefault();
+    const searchButton = document.getElementById("search");
+    searchButton.setAttribute("disabled", true)
     const form = document.getElementById("form");
     const data = Object.fromEntries(new FormData(form));
-    const response = fetch(`/search?q=${data.query}`).then((response) => {
+    const query = data.query;
+    if (!query || query.length === 0) {
+      alert("Please enter a search query")
+      return
+    }
+    const response = fetch(`/search?q=${query}`).then((response) => {
+      if (response.status !== 200) {
+        response.text().then((results) => {
+          searchButton.removeAttribute("disabled")
+          alert(results)
+        })
+        return;
+      }
       response.json().then((results) => {
-        Controller.updateTable(results);
+        Pagination.resetPagination(query);
+        Controller.updateTable(query, results);
+        searchButton.removeAttribute("disabled")
       });
     });
   },
 
-  updateTable: (results) => {
+  loadMore: (ev) => {
+    ev.preventDefault();
+    const loadMoreButton = document.getElementById("load-more");
+    loadMoreButton.setAttribute("disabled", true)
+    const response = fetch(`/search?q=${Pagination.query}&l=${Pagination.limit}&o=${Pagination.offset}`).then((response) => {
+      if (response.status !== 200) {
+        response.text().then((results) => {
+          loadMore.removeAttribute("disabled")
+          alert(results)
+        })
+        return;
+      }
+      response.json().then((results) => {
+        Pagination.increasePagination()
+        Controller.updateTable(Pagination.query, results, true);
+        loadMore.removeAttribute("disabled")
+      });
+    });
+  },
+
+  updateTable: (query, results, appendResults = false) => {
     const table = document.getElementById("table-body");
     const rows = [];
     for (let result of results) {
-      rows.push(`<tr><td>${result}</td></tr>`);
+      const markedText = result.replace(new RegExp(query, 'gi'), `<mark>${query}</mark>`);
+      rows.push(`<tr><td>${markedText}</td></tr>`);
     }
-    table.innerHTML = rows;
+    if (appendResults) {
+      table.innerHTML +=  rows;
+    } else {
+      table.innerHTML = rows;
+    }
   },
 };
 
 const form = document.getElementById("form");
 form.addEventListener("submit", Controller.search);
+const loadMore = document.getElementById("load-more");
+loadMore.addEventListener("click", Controller.loadMore);
